894032dcc589b9f6438ebb7cba0560a2ca7f2aab
marwhals <marjan.mubarok2012@gmail.com>
2023-09-25 23:31:12 +0100

    43 - Throttling

--- src/main/scala/section4_effectful/Timed.scala
+++ src/main/scala/section4_effectful/Timed.scala
@@ -6,12 +6,25 @@ import scala.concurrent.duration._
 
 object Timed extends IOApp.Simple {
   override def run: IO[Unit] = {
-    val drinkWater = Stream.iterateEval(1)(n => IO.println("Drink more water").as(n+1))
+    val drinkWater = Stream.iterateEval(1)(n => (IO.sleep(1500.millis)) *> IO.println("Drink more water").as(n+1))
 
     drinkWater.compile.drain
     drinkWater.timeout(1.second).compile.drain // Times out after one second and the execution ends with an exception
     drinkWater.interruptAfter(1.second).compile.drain // Times out after one second and the execution ends the execution gracefully
     drinkWater.delayBy(2.seconds).interruptAfter(4.seconds).compile.drain // Delay by two seconds and then run for two more seconds
+
+    // Thorttling
+    drinkWater
+      .metered(1.second)
+      .interruptAfter(5.seconds)
+      .compile
+      .drain
+
+    drinkWater
+      .meteredStartImmediately(1.second) //Compensates the time it take for the effect to run unless the time of the effect is longer than the time specified ???_???
+      .interruptAfter(5.seconds)
+      .compile
+      .drain
 
   }
 }
